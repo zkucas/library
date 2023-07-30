127a55efe5d3a5dcb661b83869daea0054ee6985
Ziggy <ziggy.kucas@gmail.com>
2023-07-29 19:29:53 -0700

    Completed the core functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 let container = document.querySelector(".container")
 let btn = document.querySelector(".add")
+let btn2 = document.querySelector(".popup-form")
+let form = document.querySelector(".form-box")
 
 let myLibrary = [];
 
@@ -13,29 +15,55 @@ function Book(author, name, pages, read){
 
 
 function addBookToLibrary(book){
+
         const newDivAuthor = document.createElement('p');
         const newDivTitle = document.createElement('p');
         const newDivPages = document.createElement('p');
 
-        const newCheckbox = document.createElement('INPUT');
-        newCheckbox.setAttribute("type", "checkbox");
-        const checkBoxLabel = document.createElement('label');
-        checkBoxLabel.appendChild(document.createTextNode('Have read it!'))
+
+        const switchDiv = document.createElement('div');
+        switchDiv.classList.add('readStatus');
+        const switchP = document.createElement('p');
+        switchP.textContent = "Read Status:";
+        const switchx = document.createElement('label');
+        switchx.classList.add('switch');
+        const switchinput = document.createElement('input');
+        switchinput.type = "checkbox";
+        const switchslider = document.createElement('span');
+        switchslider.classList.add('slider');
+
+        switchx.appendChild(switchinput)
+        switchx.appendChild(switchslider)
+        switchDiv.appendChild(switchP)
+        switchDiv.appendChild(switchx)
+      
+
+        const removeBtn = document.createElement('button')
+        removeBtn.type='button';
+        removeBtn.textContent = 'Remove';
+        removeBtn.classList.add('remBtn');
 
         const newDiv = document.createElement('div')
+        newDiv.classList.add('mainDiv');
 
         newDivAuthor.textContent = 'Book Author: ' + book.author
         newDivTitle.textContent = 'Book Title: ' + book.name
         newDivPages.textContent = 'Book Pages: ' + book.pages
+        switchinput.checked = book.read
 
 
         newDiv.appendChild(newDivAuthor)
         newDiv.appendChild(newDivTitle)
         newDiv.appendChild(newDivPages)
-        newDiv.appendChild(newCheckbox)
-        newDiv.appendChild(checkBoxLabel)
+        newDiv.appendChild(switchDiv)
+        newDiv.appendChild(removeBtn)
 
         container.appendChild(newDiv)
+
+
+        removeBtn.addEventListener('click', () =>{
+            newDiv.remove();
+        })
    
 }
 
@@ -47,19 +75,28 @@ function createForm(event){
     const author = document.querySelector(".auth").value
     const name = document.querySelector(".title").value
     const pages = document.querySelector(".pages").value
+    const read = document.querySelector(".readbox").checked
 
-    const book = new Book(author,name,pages)
+    const book = new Book(author,name,pages,read)
 
     myLibrary.push(book)
 
     addBookToLibrary(book)
 }
 
-const tempBook = new Book("Tolkien","The Hobbit", 392);
+
+function popup(){
+    form.style.display = "flex";
+}
+
+const tempBook = new Book("Tolkien","The Hobbit", 392, true);
 
 addBookToLibrary(tempBook)
 
 /*addBookToLibrary('Tolkien', 'Hobbit', '293')*/
+
+btn2.addEventListener("click", popup)
+
 
 btn.addEventListener("click", createForm)
 
